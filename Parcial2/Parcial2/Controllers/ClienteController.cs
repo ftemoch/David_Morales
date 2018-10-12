b79f791332b6c67f815a436ae83fe3496dadbeb9
davidmorales10 <camilomorales321@gmail.com>
2018-10-12 10:26:23 -0500

    profe termine todo el parcial

---
+++ Parcial2/Parcial2/Controllers/ClienteController.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using Parcial2.Models;
+
+namespace Parcial2.Controllers
+{
+    public class ClienteController : Controller
+    {
+        // GET: Cliente
+        public ActionResult Random()
+        {
+            var Cliente = new Cliente()
+            {
+                Nombre = "julieta",
+                Apellido ="vanegas",
+                Sueldo = 500000
+            };
+            return View(Cliente);
+        }
+    }
+}
