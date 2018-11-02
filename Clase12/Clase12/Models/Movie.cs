34e2fe2f466b271f4e4c49a01434b75580118cc2
davidmorales10 <camilomorales321@gmail.com>
2018-11-01 19:47:13 -0500

    algunos cambios mas

--- Clase12/Clase12/Models/Movie.cs
+++ Clase12/Clase12/Models/Movie.cs
@@ -9,9 +9,8 @@ namespace Clase12.Models
     public class Movie
     {
         public int ID { get; set; }
-       
         public String Nombre { get; set; }
-
+        
         public TipoPelicula tipoPelicula { get; set; }
         public byte IdTipoPelicula { get; set; }
     }
